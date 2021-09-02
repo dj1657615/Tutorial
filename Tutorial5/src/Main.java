07a8148931fd8f4abd3e57e126d345332ceaf79d
dj <dj@naver.com>
2021-09-02 16:07:32 +0900

    Tutorial5

---
+++ Tutorial5/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String a = "I Love YOU.";
+		if(a.contains("LoveT"))
+		{
+			System.out.println("Me Too");
+		}
+		else
+		{
+			
+			System.out.println("I Hate You");	
+			}
+	}
+
+}
+
