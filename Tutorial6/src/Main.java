cc8b67b91976817dd50849d4aadb536ccd4296f9
dj <dj@naver.com>
2021-09-06 20:03:25 +0900

    Tutorial6

---
+++ Tutorial6/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int score = 60;
+		
+		if(score >= 90)
+		{
+			System.out.println("A+입니다.");
+		}
+		else if(score >=80)
+		{
+			System.out.println("B+입니다.");
+		}
+		else if(score >=70)
+		{
+			System.out.println("c+입니다.");
+		}
+		else
+		{
+			System.out.println("d+입니다.");
+		}
+	}
+
+}
