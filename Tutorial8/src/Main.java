273195e909a0bdf469a09a3626490ddee0c69f72
dj <dj@naver.com>
2021-09-06 20:59:20 +0900

    Tutorial8

---
+++ Tutorial8/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+System.out.println("¿Ãµø¡ÿ");
+		
+		System.out.println(10 + 10);
+		System.out.println(30 * 20);
+		System.out.println(10 - 5);
+		System.out.println(50 / 5);
+		System.out.println(395 % 18);
+		
+		
+		for(int i = 0; i < 10; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				System.out.print("°⁄");
+			}
+			System.out.println();
+		}
+	}
+	
+}
